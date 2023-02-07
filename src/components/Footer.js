30cc99a202d1e6d392ea2cf1184342c7e18796ff
Havochalo <officialmail85@gmail.com>
2023-02-07 20:01:00 +1100

    frontend initialised

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Footer = () => {
-  return (
-    <div className="p-5 bg-dark text-center text-white">
-      &copy; All right reserved
-    </div>
-  );
-};
